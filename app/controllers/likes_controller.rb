76a227e57f8fefafe2831d679cb8b75855c6a806
git-kaz <okw.kzk@gmail.com>
2026-01-07 09:08:39 +0900

    fix: like機能のturbo簡略化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,37 +5,12 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.build(quote: @quote)
-
-    if @like.save
-      respond_to do |format|
-        format.html { redirect_to @quote, notice: 'いいねしました！'}
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(
-            "like-button-#{@quote.id}",
-            partial: 'likes/like_button',
-            locals: { quote: @quote }
-          )
-        end
-      end
-    else
-      redirect_to @quote, alert: 'いいねできません'
-    end
+    @like.save
   end
 
   def destroy
     @like = current_user.likes.find_by(quote: @quote)
     @like.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to @quote, notice: 'いいねを取り消しました' }
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace(
-          "like-button-#{@quote.id}",
-          partial: 'likes/like_button',
-          locals: { quote: @quote }
-        )
-      end
-    end
   end
 
   private
